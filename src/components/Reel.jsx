b302efb6a218592cebe3ad7d40836218fc43d89d
Carlos Santiago <66339441+casantiagom@users.noreply.github.com>
2023-03-07 14:43:34 -0800

    allow full screen

--- src/components/Reel.jsx
+++ src/components/Reel.jsx
@@ -11,7 +11,11 @@ const Reel = () => {
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowfullscreen="allowfullscreen"
+        mozallowfullscreen="mozallowfullscreen" 
+        msallowfullscreen="msallowfullscreen" 
+        oallowfullscreen="oallowfullscreen" 
+        webkitallowfullscreen="webkitallowfullscreen"
       ></iframe>
     </div>
   )
